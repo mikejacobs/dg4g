2c231ad9b0ee16f8e88de1cf76edc6515654fdbb
Michael Jacobs <mike@mjacobs.me>
2019-02-03 08:17:51 -0800

    removed yahoo

--- js.js
+++ js.js
@@ -1,60 +1,103 @@
 // TODO!
 // Sorting by location, type, and role
 
-var xmlURL = "https://groups.google.com/forum/feed/design-gigs-for-good/topics/rss.xml?num=50"
-var yql = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from xml where url="' + xmlURL + '"') + '&format=xml&callback=?';
-$.getJSON(yql, xmlParser);
+var xmlURL =
+  "https://groups.google.com/forum/feed/design-gigs-for-good/topics/rss.xml?num=100";
+
+google.load("feeds", "1");
+google.setOnLoadCallback(() => {
+  var feed = new google.feeds.Feed(xmlURL);
+  feed.load(xmlParser);
+});
 
 function xmlParser(xml) {
+  var $list = $("#list");
+  $("#loading").hide();
+  if (!xml.error) {
+    // console.log("xml", xml);
+    xml.feed.entries.forEach(function(item) {
+      //   console.log("item", item);
+      var parsedTitle = parseMetaData(item.title);
+      var parsedDesc = parseLinkFromDescription(item.content);
+      var $list_item = $("<div class='item'></div>");
 
-	var $list = $("#list")
-	xmlDoc = $.parseXML( xml.results[0] )
-	$xml = $( xmlDoc )
-	var $items = $xml.find( "item" );
-	$("#loading").hide()
+      $list_item.append(
+        $(
+          '<span class="meta">' +
+            parsedTitle.type +
+            "</span> &middot; " +
+            '<span class="meta">' +
+            parsedTitle.location +
+            "</span> "
+        )
+      );
+      $list_item.append(
+        $('<span class="date">' + formatDate(item.publishedDate) + "</span>")
+      );
+      $list_item.append(
+        $(
+          '<a href="' +
+            (parsedDesc.link || item.link) +
+            '" class="title">' +
+            parsedTitle.text +
+            "</a>"
+        )
+      );
+      $list_item.append(
+        $(
+          "<p>" +
+            parsedDesc.text +
+            // '... <a class="more" href=' +
+            // item.link +
+            // ">original post</a>
+            "</p>"
+        )
+      );
 
-	$items.each(function(item){
-
-		var $item = $($items[item])
-		console.log($item)
-		var parsedTitle = parseMetaData($item.find("title").text())
-		var parsedDesc = parseLinkFromDescription($item.find("description").text())
-		var $list_item = $("<div class='item'></div>")
-
-		$list_item.append($('<span class="meta">' + parsedTitle.type + '</span> &middot; ' + '<span class="meta">' + parsedTitle.location + '</span> '))
-		$list_item.append($('<span class="date">'+ formatDate($item.find("pubDate").text()) +'</span>'))
-		$list_item.append($('<a href="'+ ( parsedDesc.link || $item.find("link").text() )+'" class="title">'+parsedTitle.text+'</a>'))
-		$list_item.append($('<p>' + parsedDesc.text + '... <a class="more" href='+$item.find("link").text()+'>original post</a></p>'))
-
-		$list.append($list_item)
-	})
+      $list.append($list_item);
+    });
+  } else {
+    $list.append(
+      $("<div>Sorry :( Could not retrieve job listings at the time.</div>")
+    );
+  }
 }
 
-function formatDate(str){
-
-	var d = new Date(str)
-	var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-	return months[d.getMonth()] + " " + d.getDate()
+function formatDate(str) {
+  var d = new Date(str);
+  var months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
+  return months[d.getMonth()] + " " + d.getDate();
 }
 
-function parseLinkFromDescription(desc){
+function parseLinkFromDescription(desc) {
+  var link = desc.match(/\bhttps?:\/\/\S+/gi);
+  var text = desc.replace(/\bhttps?:\/\/\S+/gi, "").replace(" // ", "");
 
-	var link = desc.match(/\bhttps?:\/\/\S+/gi)
-	var text = desc.replace(/\bhttps?:\/\/\S+/gi, '').replace(" // ", "")
-
-	return {
-		text:text,
-		link:link
-	}
+  return {
+    text: text,
+    link: link
+  };
 }
 
-function parseMetaData(title){
-
-	var parts = title.split("]")
-	var meta = parts[0].split(", ")
-	return {
-		text:parts[1],
-		location:meta[1],
-		type:meta[0].split("[")[1]
-	}
+function parseMetaData(title) {
+  var parts = title.split("]");
+  var meta = parts[0].split(", ");
+  return {
+    text: parts[1],
+    location: meta[1],
+    type: meta[0].split("[")[1]
+  };
 }
